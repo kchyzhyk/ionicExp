acfca399bdc499a1b1b0fcdc83d7558bd9809dfd
kchyzhyk <kchyzhykova888@gmail.com>
2022-06-16 16:45:33 +0200

    update

--- src/app/pages/movie-details/movie-details.page.ts
+++ src/app/pages/movie-details/movie-details.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {MovieService} from '../../services/movie.service';
+import {environment} from 'src/environments/environment';
 
 @Component({
   selector: 'app-movie-details',
@@ -6,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./movie-details.page.scss'],
 })
 export class MovieDetailsPage implements OnInit {
+  movie = null;
+  imageBaseUrl = environment.images;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private movieService: MovieService) {
+
+  };
 
   ngOnInit() {
+    const id = this.route.snapshot.paramMap.get('id');
+    this.movieService.getMovieDetails(id).subscribe(res => {
+      this.movie = res;
+    });
   }
 
 }
